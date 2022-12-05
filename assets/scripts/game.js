a48a083d24498eedf3c810fb08bb379b67793359
Niromash <matteo.chretien@supinfo.com>
2022-12-05 12:51:24 +0100

    Fix board

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,17 +1,35 @@
 const board = document.querySelector('.board');
+
 const table = [];
-let columns = getCookie('columns');
-let rows = getCookie('rows');
-let player = true;
+let scores = {
+    yellow: 0,
+    red: 0,
+    max: 3
+}
+let playerTurn = "yellow";
+let columns = Number(getCookie('columns'));
+let rows = Number(getCookie('rows'));
 let light = document.querySelector('.who-is-playing');
 document.querySelector("#y").textContent = columns;
 document.querySelector("#x").textContent = rows;
 document.querySelector(".board").style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
 document.querySelector(".board").style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+let startTime = Date.now();
+updateTime();
+setInterval(updateTime, 1000);
 
-for (let i = 0; i < columns; i++) {
+
+// The time must be minutes:seconds
+function updateTime() {
+    let time = Date.now() - startTime;
+    let seconds = Math.floor((time / 1000) % 60);
+    let minutes = Math.floor((time / (1000 * 60)) % 60);
+    document.querySelector("#time").textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+for (let i = 0; i < rows; i++) {
     const row = [];
-    for (let j = 0; j < rows; j++) {
+    for (let j = 0; j < columns; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell', 'empty');
         cell.dataset.row = i;
@@ -19,46 +37,18 @@ for (let i = 0; i < columns; i++) {
         row.push(cell);
         cell.appendChild(document.createElement('div'));
         board.appendChild(cell);
-        cell.addEventListener('click', () => {
-            if (!cell.classList.contains('empty')) {
-                return;
-            }
-            if (player) {
-                set(j, i, 'yellow');
-                light.classList.toggle('red');
+        cell.addEventListener("click", () => {
+            const oldPlayerTurn = playerTurn;
+            playerTurn = play(cell.dataset.column, playerTurn);
+            if (playerTurn !== oldPlayerTurn) {
                 light.classList.toggle('yellow');
-                console.log(check());
-                player = false;
-            } else {
-                set(j, i, 'red');
                 light.classList.toggle('red');
-                light.classList.toggle('yellow');
-                console.log(check());
-                player = true;
             }
+            // Timeout to wait that the animation is finished
+            setTimeout(checkVictory, 100);
         })
     }
     table.push(row);
-}
-
-function check() {
-    for (let i = 0; i < columns; i++) {
-        for (let j = 0; j < rows; j++) {
-            if (table[i][j].classList.contains('empty')) {
-                continue;
-            }
-            const color = table[i][j].classList[1];
-            // check horizontal
-            if (j + 3 < rows && color === table[i][j + 1].classList[1] && color === table[i][j + 2].classList[1] && color === table[i][j + 3].classList[1]) {
-                return true;
-            }
-            // check vertical
-            if (i + 3 < columns && color === table[i + 1][j].classList[1] && color === table[i + 2][j].classList[1] && color === table[i + 3][j].classList[1]) {
-                return true;
-            }
-        }
-
-    }
 }
 
 function set(x, y, color) {
@@ -66,6 +56,117 @@ function set(x, y, color) {
     table[y][x].classList.add(color);
 }
 
+function play(x, color) {
+    for (let y = (rows - 1); y >= 0; y--) {
+        if (table[y][x].classList.contains('empty')) {
+            set(x, y, color);
+            color = color === "yellow" ? "red" : "yellow";
+            return color;
+        }
+    }
+
+    alert("La colonne est pleine");
+    return color;
+}
+
+// This function check if there is a winner (4 same color in a row)
+function checkVictory() {
+    const resetTable = () => table.forEach(row => row.forEach(cell => {
+        cell.classList.remove('red');
+        cell.classList.remove('yellow');
+        cell.classList.add('empty');
+    }));
+
+    const win = color => {
+        scores[color]++;
+        document.querySelector(`#${color}-score`).textContent = scores[color];
+        resetTable();
+        alert(`Le joueur ${color} a gagné la manche !`);
+        if (scores[color] >= scores.max) {
+            alert(`Le joueur ${color} a gagné la partie !`);
+            sendScore();
+            document.querySelector(`#yellow-score`).textContent = 0;
+            document.querySelector(`#red-score`).textContent = 0;
+            scores = {
+                yellow: 0,
+                red: 0,
+                max: scores.max
+            }
+        }
+    }
+
+    // Check horizontal
+    for (let y = 0; y < columns - 1; y++) {
+        for (let x = 0; x < 4; x++) {
+            if (table[y][x].classList.contains('yellow') && table[y][x + 1].classList.contains('yellow') && table[y][x + 2].classList.contains('yellow') && table[y][x + 3].classList.contains('yellow')) {
+                win('yellow');
+            }
+            if (table[y][x].classList.contains('red') && table[y][x + 1].classList.contains('red') && table[y][x + 2].classList.contains('red') && table[y][x + 3].classList.contains('red')) {
+                win('red');
+            }
+        }
+    }
+    // Check vertical
+    for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < columns; x++) {
+            if (table[y][x].classList.contains('yellow') && table[y + 1][x].classList.contains('yellow') && table[y + 2][x].classList.contains('yellow') && table[y + 3][x].classList.contains('yellow')) {
+                win('yellow');
+            }
+            if (table[y][x].classList.contains('red') && table[y + 1][x].classList.contains('red') && table[y + 2][x].classList.contains('red') && table[y + 3][x].classList.contains('red')) {
+                win('red');
+            }
+        }
+    }
+    // Check diagonal
+    for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < 4; x++) {
+            if (table[y][x].classList.contains('yellow') && table[y + 1][x + 1].classList.contains('yellow') && table[y + 2][x + 2].classList.contains('yellow') && table[y + 3][x + 3].classList.contains('yellow')) {
+                win('yellow');
+            }
+            if (table[y][x].classList.contains('red') && table[y + 1][x + 1].classList.contains('red') && table[y + 2][x + 2].classList.contains('red') && table[y + 3][x + 3].classList.contains('red')) {
+                win('red');
+            }
+        }
+    }
+
+    // Check diagonal
+    for (let y = 0; y < 3; y++) {
+        for (let x = 3; x < columns; x++) {
+            if (table[y][x].classList.contains('yellow') && table[y + 1][x - 1].classList.contains('yellow') && table[y + 2][x - 2].classList.contains('yellow') && table[y + 3][x - 3].classList.contains('yellow')) {
+                win('yellow');
+            }
+            if (table[y][x].classList.contains('red') && table[y + 1][x - 1].classList.contains('red') && table[y + 2][x - 2].classList.contains('red') && table[y + 3][x - 3].classList.contains('red')) {
+                win('red');
+            }
+        }
+    }
+}
+
+if (!getCookie("hostId")) {
+    fetch('https://power4-api.tolnkee.com/generateUserId', {method: 'GET'}).then(resp => resp.text()).then(id => {
+        document.cookie = `hostId=${id}; path=/`;
+    })
+}
+
+const sendScore = () => {
+    fetch('https://power4-api.tolnkee.com/score', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            playerRed: scores.red,
+            playerYellow: scores.yellow,
+            boardSizeX: columns,
+            boardSizeY: rows,
+            hostId: getCookie("hostId")
+        }),
+    }).then(async resp => {
+        if (resp.status !== 200) {
+            console.error("Error while sending score", await resp.text());
+        }
+    })
+}
 
 function getCookie(name) {
     let cookie = {};
@@ -75,10 +176,3 @@ function getCookie(name) {
     })
     return cookie[name];
 }
-
-
-// set(0, 0, 'yellow');
-// set(1, 0, 'yellow');
-// set(2, 0, 'yellow');
-// set(3, 0, 'yellow');
-// set(4, 0, 'red');
